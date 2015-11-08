fcdc9333e607b305d9c1ad64c69b39803e5a3363
Ethan <ethanabrooks@gmail.com>
2015-11-07 21:16:24 -0500

    merged

---
+++ testNeuralNetDigits.py
@@ -0,0 +1,19 @@
+from numpy import loadtxt
+from sklearn.cross_validation import train_test_split, cross_val_score, KFold
+from nn import NeuralNet
+
+if __name__ == "__main__":
+    # Load Data
+    filename = 'data/digitsX.dat'
+    X = loadtxt(filename, delimiter=',')[:400]
+    filename = 'data/digitsY.dat'
+    y = loadtxt(filename)[:400]
+    n = len(y)
+    kf = KFold(n, n_folds=4)
+
+    scores = []
+    for train_index, test_index in kf:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        nn = NeuralNet(1)
+        scores.append(nn.score(X_train, y_train, X_test, y_test))
